3d51d88602ab4fe93ca9c4df7c75e6b8ddadd121
Sudhanshu Jayant <sudhanshukrishna3@gmail.com>
2023-12-27 12:54:19 +0530

    Initial Commit

---
+++ config/key.js
@@ -0,0 +1,19 @@
+module.exports = {
+    MongoURI: "mongodb+srv://sudhanshukrishna3:nWkrppvdUD65mtJL@sudhanshu.bm2sktl.mongodb.net/"
+}
+
+// // mongodb://127.0.0.1:27017/codeial
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://127.0.0.1:27017/Nodejs-Auth');//
+
+// const db = mongoose.connection;
+
+// db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
+
+// db.once('open',function(){
+//     console.log("Connected to Database :: MongoDB");
+// });
+
+// module.exports = db;
